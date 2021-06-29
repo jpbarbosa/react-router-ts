1e9a45311d6c4e028e8a859da155544f43d87ff7
JP Barbosa <jp@jp7.com.br>
2021-06-29 20:43:35 -0300

    Add app description to home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const HomeIndex: React.FC = () => {
   return (
     <div className="content">
       <h2>Home</h2>
+      <div>
+        <p>This is a simple example showing how to use React Router:</p>
+        <ul>
+          <li>Home (this page)</li>
+          <li>
+            <Link to="/tasks">Tasks (/tasks)</Link>
+          </li>
+          <li>
+            <Link to="/tasks/new">New Task (/tasks/new)</Link>
+          </li>
+          <li>Edit Task (/tasks/TASK_ID/edit)</li>
+        </ul>
+      </div>
     </div>
   );
 };
